3f956161ef632d9a86e56ace1e52c8f307796072
joao vitor dourado freire santos <joaovitor20062006@gmail.com>
2026-03-30 19:32:09 -0300

    questao apagando

--- ed/apagando/go/draft.go
+++ ed/apagando/go/draft.go
@@ -1,14 +1,26 @@
 package main
+
 import "fmt"
+
 func main() {
 	var qtdInicial int
 	fmt.Scan(&qtdInicial)
 	pessoasFila := make([]int, qtdInicial)
-	for   i:= 0 ; i< qtdInicial ; i++{
+	for i := 0; i < qtdInicial; i++ {
 		fmt.Scan(&pessoasFila[i])
 	}
 	qntSai := 0
 	fmt.Scan(&qntSai)
-
-
+	vouTirar := make(map[int]bool)
+	for i := 0; i < qntSai; i++ {
+		var idSaindo int
+		fmt.Scan(&idSaindo)
+		vouTirar[idSaindo] = true
+	}
+	for i := 0; i < qtdInicial; i++ {
+		if !vouTirar[pessoasFila[i]] {
+			fmt.Printf("%v ", pessoasFila[i])
+		}
+	}
+	fmt.Println()
 }
